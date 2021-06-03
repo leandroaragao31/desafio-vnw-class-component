b91c1478c3f6353c960531058ddf32c5a0c45df9
Leandro de Aragão Cezar <71722185+leandroaragao31@users.noreply.github.com>
2021-06-03 00:20:55 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./styles.css";
+
+class Mensagem extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="bencao">Que vocês tenham um dia abençoado!</h1>
+      </div>
+    );
+  }
+}
+
+export default Mensagem;
